11be614bd7ce2fee66b41794e2a531f0ef26d198
Ninth-Crusade <jpabo2007@gmail.com>
2025-02-11 10:23:17 -1000

    Create plusTwo.java

---
+++ Coding_Bat_Arrays_Three/plusTwo.java
@@ -0,0 +1,10 @@
+public int[] plusTwo(int[] a, int[] b) {
+  int temp1 = a[0];
+  int temp2 = a[1];
+  int temp3 = b[0];
+  int temp4 = b[1];
+  
+  int [] tempArr = {temp1, temp2, temp3, temp4};
+  
+  return tempArr;
+}
